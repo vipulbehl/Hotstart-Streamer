2e06aa0c1f832727bfbb3fc96e663ae5450f5054
biezom <bolivebiezom@free.fr>
2015-10-20 12:03:20 +0200

    add play or download

--- starsportslivestreamer.bat
+++ starsportslivestreamer.bat
@@ -4,6 +4,7 @@ call set folder="%~dp0\videos\\"
 call set livestreamer="%~dp0\tools\livestreamer\\"
 call "%~dp0\tools\php5.4\php.exe" starsportslivestreamer.php "%%link%%" "%%livestreamer%%"
 call set /p quality=write quality (example write 720p):
-call "%~dp0\tools\php5.4\php.exe" starsportslivestreamer.php "%%link%%" "%%quality%%" "%%folder%%" "%%livestreamer%%"
+call set /p choice=play or download? (write p or d):
+call "%~dp0\tools\php5.4\php.exe" starsportslivestreamer.php "%%link%%" "%%quality%%" "%%folder%%" "%%livestreamer%%" "%%choice%%"
 pause
 :end
