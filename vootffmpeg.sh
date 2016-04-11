ee9d2f82cf8027986220d2f1bd6b5887951ad9a6
biezom <bolivebiezom@free.fr>
2016-04-11 02:36:17 +0200

    Update vootffmpeg.sh add max bitrate

--- vootffmpeg.sh
+++ vootffmpeg.sh
@@ -3,11 +3,10 @@ echo "paste the link"
 read link
 
 folder=$PWD/videos/
-livestreamer=""
-ffmpeg=""
 
-echo "play or download? (write low medium or high)"
+ffmpeg=""
+php vootffmpeg.php "$link" "$folder" "$ffmpeg"
+echo "write bitrate"
 read quality
 
-php vootffmpeg.php "$link" "$folder" "$livestreamer" "$quality" "$ffmpeg"
-fi
+php vootffmpeg.php "$link" "$folder" "$ffmpeg" "$quality" 
