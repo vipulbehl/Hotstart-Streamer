7dbd3a7964a1f5b1a9caced1b4cc0c2c820ebf1c
biezom <bolivebiezom@free.fr>
2016-04-11 02:31:00 +0200

    Update voot.php add max bitrate

--- vootffmpeg.bat
+++ vootffmpeg.bat
@@ -1,10 +1,10 @@
 @ echo off
 call set /p link=paste the link:
 call set folder="%~dp0\videos\\"
-call set /p quality=write quality (write low medium or high):
-call set livestreamer="%~dp0\tools\livestreamer\\"
 call set ffmpeg="%~dp0\tools\ffmpeg\\"
-call "%~dp0\tools\php5.4\php.exe" vootffmpeg.php "%%link%%" "%%folder%%" "%%livestreamer%%" "%%quality%%" "%%ffmpeg%%"
+call "%~dp0\tools\php5.4\php.exe" vootffmpeg.php "%%link%%" "%%folder%%" "%%ffmpeg%%"
+call set /p quality=write bitrate:
+call "%~dp0\tools\php5.4\php.exe" vootffmpeg.php "%%link%%" "%%folder%%" "%%ffmpeg%%" "%%quality%%"
 :end1
 pause
 :end
